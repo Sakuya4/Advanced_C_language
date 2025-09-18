f46ca6ba9641f6d0c075f0d8b3d3a83586656b23
Tsai <155059505+Sakuya4@users.noreply.github.com>
2025-09-19 01:40:48 +0800

    Create bubbleSortUsingPassByValue.c

---
+++ practiceOfSimpleC/bubbleSortUsingPassByValue.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+# define SIZE 10
+
+void bubbleSort(int *const array, size_t size);
+void swap(int *element1Ptr, int *element2Ptr);
+
+int main(void)
+{
+  int a[SIZE]={2,6,4,8,10,12,89,68,45,37};
+  puts("Data items in original order");
+  for(size_t i=0;i<SIZE;++i) printf("%4d", a[i]);
+  bubbleSort(a, SIZE);
+  puts("\nData items in sorted order");
+  for(size_t i=0;i<SIZE;++i) printf("%4d", a[i]);
+  puts("");
+}
+
+
+void bubbleSort(int *const array, size_t size)
+{
+//   void swap(int *element1Ptr, int *element2Ptr);// prototype
+  for(int pass=0;pass<size-1;++pass)
+  {
+    for(size_t j=0;j<size-1;++j)
+    {
+      if(array[j]>array[j+1]) swap(&array[j], &array[j+1]);
+    }
+  }
+}
+
+void swap(int *element1Ptr, int *element2Ptr)
+{
+  int hold=*element1Ptr;
+  *element1Ptr=*element2Ptr;
+  *element2Ptr=hold;
+}
